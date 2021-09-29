694980653e5fe56c14c167577b5a35c4f729ccbe
unknown <Anatoly>
2021-09-29 16:39:56 +0300

    bundling

--- src/index.tsx
+++ src/index.tsx
@@ -29,10 +29,14 @@ const App = () => {
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()]
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production',
+                global: 'window'
+            }
         })
         console.log(res)
-        setCode(res.code)
+        setCode(res.outputFiles[0].text)
     }
 
     return <div>
